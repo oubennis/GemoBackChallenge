5254352ac894e549ddfda96a624263cc16da03a1
oubennis <oubennis@gmail.com>
2020-09-13 17:54:37 +0100

    first commit

---
+++ src/main/java/com/obf/trendrepos/TrendReposApplication.java
@@ -0,0 +1,13 @@
+package com.obf.trendrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendReposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendReposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obf/trendrepos/TrendReposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obf.trendrepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendReposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
